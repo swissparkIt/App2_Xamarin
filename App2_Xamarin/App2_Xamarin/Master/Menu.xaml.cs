e1da5b9d4111b3a534ff0dc805d5c62d5626388a
julio <julio@DESKTOP-HEHU36U>
2019-03-08 00:17:44 -0300

    casa - commit1

---
+++ App2_Xamarin/App2_Xamarin/Master/Menu.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2_Xamarin.Master
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Menu : MasterDetailPage
+	{
+		public Menu ()
+		{
+			InitializeComponent ();
+		}
+
+        private void GoPagPerfil1(object sender, EventArgs args)
+        {
+            //MainPage = new App2_Xamarin.Pages.Perfil1();
+        }
+
+        private void GoPagXamarin(object sender, EventArgs args)
+        {
+
+        }
+
+    }
+}
