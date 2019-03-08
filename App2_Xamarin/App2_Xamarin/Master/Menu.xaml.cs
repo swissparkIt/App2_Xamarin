b86ce92ed821e190762c73b0ee0ab4a91c683626
julio <juliodisouza@gmail.com>
2019-03-08 12:59:14 -0300

    swiss - aulas 08/03/2019

--- App2_Xamarin/App2_Xamarin/Master/Menu.xaml.cs
+++ App2_Xamarin/App2_Xamarin/Master/Menu.xaml.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -19,12 +14,12 @@ namespace App2_Xamarin.Master
 
         private void GoPagPerfil1(object sender, EventArgs args)
         {
-            //MainPage = new App2_Xamarin.Pages.Perfil1();
+            Navigation.PushAsync(new Pages.Perfil1());
         }
 
         private void GoPagXamarin(object sender, EventArgs args)
         {
-
+            Navigation.PushAsync(new Pages.Xamarin());
         }
 
     }
