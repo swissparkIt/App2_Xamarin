0443870b5d51f066268f9f5f85507ac7eb40bfd6
julio <julio@DESKTOP-HEHU36U>
2019-03-09 15:25:28 -0300

    casa - Ajustes de scrollview para as páginas de perfil e sobre.

--- App2_Xamarin/App2_Xamarin/Master/Menu.xaml.cs
+++ App2_Xamarin/App2_Xamarin/Master/Menu.xaml.cs
@@ -15,13 +15,17 @@ namespace App2_Xamarin.Master
         private void GoPagPerfil1(object sender, EventArgs args)
         {
             //Navigation.PushAsync(new Pages.Perfil1());
-            Detail = new Pages.Perfil1();
+            //Detail = new Pages.Perfil1();
+            Detail = new NavigationPage(new Pages.Perfil1());
+            IsPresented = false;
         }
 
         private void GoPagXamarin(object sender, EventArgs args)
         {
             //Navigation.PushAsync(new Pages.Xamarin());
-            Detail = new Pages.Xamarin();
+            //Detail = new Pages.Xamarin();
+            Detail = new NavigationPage(Detail = new Pages.Xamarin());
+            IsPresented = false;
         }
 
     }
